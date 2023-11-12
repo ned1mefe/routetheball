8326e6c6a1e8911b709fb19a6c9c1c06cea88eaa
Nedim Efe Candaş <efe.candas@metu.edu.tr>
2023-11-12 23:58:08 +0300

    change background

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,12 +4,9 @@ public class Checkpoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Entered OnTriggerEnter2D");
         if (collision.gameObject.tag == "Ball")
         {
-            Debug.Log("Collided");
-            gameObject.SetActive(false);
+            gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
         }
     }
-
 }
