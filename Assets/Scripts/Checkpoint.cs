22ee501d7560550979e8d0c4afd5dbdbac24cc0d
Nedim Efe Candaş <efe.candas@metu.edu.tr>
2023-11-13 21:30:48 +0300

    change the appearance of checkpoint when reached

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite dashedSprite;
+    [SerializeField]
+    private Sprite greenSprite;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = dashedSprite;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ball")
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
+            _spriteRenderer.sprite = greenSprite;
         }
     }
 }
